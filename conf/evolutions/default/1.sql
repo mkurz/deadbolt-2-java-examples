18c6ae386908a1f38ebd5c3894d0bfd22116237f
Steve Chaloner <github-NOSPAM@objectify.be>
2012-12-15 22:41:10 +0100

    heroku...

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,67 @@
+# --- !Ups
+
+create table authorised_user (
+  id                        bigint not null,
+  user_name                 varchar(255),
+  constraint pk_authorised_user primary key (id))
+;
+
+create table security_role (
+  id                        bigint not null,
+  name                      varchar(255),
+  constraint pk_security_role primary key (id))
+;
+
+create table user_permission (
+  id                        bigint not null,
+  value                     varchar(255),
+  constraint pk_user_permission primary key (id))
+;
+
+
+create table authorised_user_security_role (
+  authorised_user_id             bigint not null,
+  security_role_id               bigint not null,
+  constraint pk_authorised_user_security_role primary key (authorised_user_id, security_role_id))
+;
+
+create table authorised_user_user_permission (
+  authorised_user_id             bigint not null,
+  user_permission_id             bigint not null,
+  constraint pk_authorised_user_user_permission primary key (authorised_user_id, user_permission_id))
+;
+create sequence authorised_user_seq;
+
+create sequence security_role_seq;
+
+create sequence user_permission_seq;
+
+
+
+
+alter table authorised_user_security_role add constraint fk_authorised_user_security_r_01 foreign key (authorised_user_id) references authorised_user (id);
+
+alter table authorised_user_security_role add constraint fk_authorised_user_security_r_02 foreign key (security_role_id) references security_role (id);
+
+alter table authorised_user_user_permission add constraint fk_authorised_user_user_permi_01 foreign key (authorised_user_id) references authorised_user (id);
+
+alter table authorised_user_user_permission add constraint fk_authorised_user_user_permi_02 foreign key (user_permission_id) references user_permission (id);
+
+# --- !Downs
+
+drop table if exists authorised_user cascade;
+
+drop table if exists authorised_user_security_role cascade;
+
+drop table if exists authorised_user_user_permission cascade;
+
+drop table if exists security_role cascade;
+
+drop table if exists user_permission cascade;
+
+drop sequence if exists authorised_user_seq;
+
+drop sequence if exists security_role_seq;
+
+drop sequence if exists user_permission_seq;
+
