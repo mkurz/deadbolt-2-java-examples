a9e9ebf4fc51a814a48c97171ca66143f2795951
Steve Chaloner <github-NOSPAM@objectify.be>
2013-03-13 18:35:44 +0100

    RC1 release

--- project/Build.scala
+++ project/Build.scala
@@ -5,12 +5,12 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "deadbolt-2-usage-java"
-    val appVersion      = "2.1-SNAPSHOT"
+    val appVersion      = "2.1-RC1"
 
     val appDependencies = Seq(
       javaCore,
       javaEbean,
-      "be.objectify" %% "deadbolt-java" % "2.1-SNAPSHOT",
+      "be.objectify" %% "deadbolt-java" % "2.1-RC1",
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
