27fcd045d427e6139511693434febbaac89673b2
Steve Chaloner <bitbucket@objectify.be>
2016-03-31 11:56:27 +0200

    moved to deadbolt 2.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,19 +5,16 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "deadbolt-2-usage-java"
-    val appVersion      = "2.2-RC4"
+    val appVersion      = "2.2.0"
 
     val appDependencies = Seq(
       javaCore,
       javaEbean,
-      "be.objectify" %% "deadbolt-java" % "2.2-RC4",
+      "be.objectify" %% "deadbolt-java" % "2.2.1",
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
-      // Change this to point to your local play repository
-      resolvers += Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
-      resolvers += Resolver.url("Objectify Play Repository - snapshots", url("http://schaloner.github.com/snapshots/"))(Resolver.ivyStylePatterns),
       resolvers += Resolver.url("typesafe", url("http://repo.typesafe.com/typesafe/repo"))
     )
 
